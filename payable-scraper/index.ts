adefdc976bd7e3ee4f9100f81614a203447f0b04
MSMazaya <msulthanmazaya@gmail.com>
2022-11-02 21:35:08 +0700

    feat: add working getBalance

--- payable-scraper/index.ts
+++ payable-scraper/index.ts
@@ -12,15 +12,16 @@ interface BankTransaction {
 }
 
 abstract class BaseBankScraper {
-    abstract login()
-    abstract getCurrentBalance(): number
-    abstract getTransactions(startAt: Date, endAt: Date): BankTransaction[]
-    abstract logout()
+    abstract login(): void
+    abstract getCurrentBalance(): Promise<number>
+    abstract getTransactions(startAt: Date, endAt: Date): Promise<BankTransaction[]>
+    abstract logout(): void
 }
 
 class BniBankScraper extends BaseBankScraper {
     accountId: string;
     password: string;
+    loggedIn: boolean;
     browser: puppeteer.Browser;
     page: puppeteer.Page;
 
@@ -28,70 +29,87 @@ class BniBankScraper extends BaseBankScraper {
         super()
         this.accountId = accountId;
         this.password = password;
+        this.loggedIn = false;
     }
 
     async init() {
-        this.browser = await puppeteer.launch();
+        this.browser = await puppeteer.launch({
+            headless: false
+        });
         this.page = await this.browser.newPage();
     }
 
     async login() {
+        try {
 
-        await this.page.goto('https://ibank.bni.co.id');
+            await this.page.goto('https://ibank.bni.co.id');
 
-        let trial = 0;
-        let captchaText = "";
-        while (trial < 3) {
-            const captcha = await this.page.$('#IMAGECAPTCHA');
-            const captchaImage = await captcha.screenshot();
+            let trial = 0;
+            let captchaText = "";
+            console.log("captcha")
+            while (trial < 20) {
+                const captcha = await this.page.$('#IMAGECAPTCHA');
+                const captchaImage = await captcha.screenshot();
 
-            const { data: { text } } = await Tesseract.recognize(
-                captchaImage,
-                'eng',
-            )
+                const { data: { text } } = await Tesseract.recognize(
+                    captchaImage,
+                    'eng',
+                )
 
-            if (text !== "") {
-                captchaText = text;
-                break;
+                if (text !== "") {
+                    captchaText = text;
+                    break;
+                }
+
+                await this.page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
+                trial += 1
             }
 
-            await this.page.reload({ waitUntil: ["networkidle0", "domcontentloaded"] });
-            trial += 1
+            console.log("typing ", trial)
+            await this.page.type('#AuthenticationFG\\.ACCESS_CODE', this.password);
+            await this.page.type('#AuthenticationFG\\.USER_PRINCIPAL', this.accountId);
+            await this.page.type('#AuthenticationFG\\.VERIFICATION_CODE', captchaText);
+
+            this.loggedIn = true;
+            console.log("loggedIn")
+        } catch {
+            await this.logout()
         }
-
-        await this.page.type('#AuthenticationFG\\.ACCESS_CODE', this.password);
-        await this.page.type('#AuthenticationFG\\.USER_PRINCIPAL', this.accountId);
-        await this.page.type('#AuthenticationFG\\.VERIFICATION_CODE', captchaText);
-
-        console.log(this.page.url());
-
-        // screenshot
-        await this.page.screenshot({ path: 'example.png' });
-
-        // await this.page.click('#VALIDATE_CREDENTIALS');
-
-        console.log(this.page.url());
-
     }
 
-    getCurrentBalance(): number {
-        /*
-        #REKENING
-        #Informasi-Saldo--Mutasi_Saldo-Rekening
-        #TRANSACTION_CATEGORIZATION
-        */
+    async getCurrentBalance(): Promise<number> {
+        try {
+            if (this.loggedIn) {
+                console.log("click rekening")
+                await this.page.waitForSelector("#REKENING");
+                await this.page.click("#REKENING")
 
-        return 0;
+                console.log("click saldo")
+                await this.page.waitForSelector("#Informasi\-Saldo\-\-Mutasi\_Saldo\-Rekening");
+                await this.page.click("#Informasi\-Saldo\-\-Mutasi\_Saldo\-Rekening")
+
+
+                console.log("eval")
+                await this.page.waitForSelector("#HREF\_AccountSummaryFG\\.BALANCE\_ARRAY\\[0\\]");
+                const currentBalance = await this.page.$eval("#HREF\_AccountSummaryFG\\.BALANCE\_ARRAY\\[0\\]", el => el.innerHTML)
+                return parseInt(currentBalance);
+            }
+            throw new Error("User is not logged in")
+        } catch {
+            await this.logout()
+        }
     }
 
-    getTransactions(startAt: Date, endAt: Date): BankTransaction[] {
+    async getTransactions(startAt: Date, endAt: Date): Promise<BankTransaction[]> {
         return [];
     }
 
     async logout() {
+        await this.page.click("#HREF_Logout")
+        await this.page.click("#LOG_OUT")
         await this.browser.close()
     }
 }
 
 const scraper = new BniBankScraper(process.env.ACCOUNT, process.env.PASSWORD);
-scraper.init().then(() => scraper.login().then(() => scraper.logout()))
+scraper.init().then(() => scraper.login().then(() => scraper.getCurrentBalance().then(() => scraper.logout())))
