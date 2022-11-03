e6f888de48e2594be8a2456b0b52ea237975f2b8
MSMazaya <msulthanmazaya@gmail.com>
2022-11-04 03:26:39 +0700

    feat: update input ui using inquirer

--- example/get-balance.ts
+++ example/get-balance.ts
@@ -1,8 +1,41 @@
 import scraper from 'payable-scraper';
+import inquirer from 'inquirer';
+
+function getCredentials() {
+    const questions = [
+        {
+            name: 'accountId',
+            type: 'input',
+            message: 'Enter your bank account id:',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your username or e-mail address.';
+                }
+            }
+        },
+        {
+            name: 'password',
+            type: 'password',
+            message: 'Enter your password:',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your password.';
+                }
+            }
+        },
+    ];
+
+    return inquirer.prompt(questions);
+}
 
 async function main() {
-    const accountId = process.env.ACCOUNT_ID;
-    const password = process.env.PASSWORD;
+    const credentials = await getCredentials();
+    const { accountId, password } = credentials;
+
     const bniScraper = await scraper().pickEngine('puppeteer').pickScraper("BNI").setCreds({ accountId, password }).build();
     await bniScraper.login()
 
--- example/get-transaction.ts
+++ example/get-transaction.ts
@@ -1,14 +1,72 @@
 import scraper from 'payable-scraper';
 import moment from 'moment';
+import inquirer from 'inquirer';
+
+function getCredentials() {
+    const questions = [
+        {
+            name: 'accountId',
+            type: 'input',
+            message: 'Enter your bank account id:',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your username or e-mail address.';
+                }
+            }
+        },
+        {
+            name: 'password',
+            type: 'password',
+            message: 'Enter your password:',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter your password.';
+                }
+            }
+        },
+        {
+            name: 'dateStart',
+            type: 'input',
+            message: 'Enter date start (DD-MM-YYYY):',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'please enter a date start';
+                }
+            }
+        },
+        {
+            name: 'dateEnd',
+            type: 'input',
+            message: 'Enter date end (DD-MM-YYYY):',
+            validate: function(value: string) {
+                if (value.length) {
+                    return true;
+                } else {
+                    return 'Please enter a date end';
+                }
+            }
+        },
+    ];
+
+    return inquirer.prompt(questions);
+}
 
 async function main() {
-    const accountId = process.env.ACCOUNT_ID;
-    const password = process.env.PASSWORD;
+    const creds = await getCredentials();
+
+    const { accountId, password, dateStart, dateEnd } = creds;
+
     const bniScraper = await scraper().pickEngine('puppeteer').pickScraper("BNI").setCreds({ accountId, password }).build();
     await bniScraper.login()
 
-    const previousDate = moment('20-10-2022', 'DD-MM-YYYY').toDate();
-    const currentDate = new Date();
+    const previousDate = moment(dateStart, 'DD-MM-YYYY').toDate();
+    const currentDate = moment(dateEnd, 'DD-MM-YYYY').toDate();
 
     const transactions = await bniScraper.getTransactions(previousDate, currentDate)
 
